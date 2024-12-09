807d47ce8282afa2a871f30cb7c140d7458169a7
mike.p6x <mike.p6x@gmail.com>
2024-12-09 16:14:58 +0700

    20241209_add_aspire

---
+++ src/Aspire/ServiceDefaults/MetricsConstants.cs
@@ -0,0 +1,13 @@
+namespace ServiceDefaults;
+
+public class MetricsConstants
+{
+    public const string AppName = "eshop-microservices";
+    public const string Basket = "Basket";
+    public const string Catalog = "Catalog";
+    public const string Discount = "Discount";
+    public const string Ordering = "Ordering";
+
+    public const string Dimensions = "Dimensions";
+    public const string EntityCodes = "EntityCodes";
+}
