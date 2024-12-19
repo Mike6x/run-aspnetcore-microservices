380f2ffc5fd55880618b5aba9a83c5bd6947609e
mike.p6x <mike.p6x@gmail.com>
2024-12-19 11:27:16 +0700

    20241219_final_vesioning_backet_api

---
+++ src/Services/Catalog/Catalog.API/Features/v2/Products/GetProductsV2Handler.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Domain;
+using Catalog.API.Features.v1.Products;
+
+namespace Catalog.API.Features.v2.Products;
+
+public record GetProductsV2Query(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
+
+internal class GetProductsV2QueryHandler
+    (IDocumentSession session)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
